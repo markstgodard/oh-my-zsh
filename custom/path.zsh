20d0fc916e7c08930f911537ef9aa0e3ea85e1fc
Mark St.Godard <markstgodard@gmail.com>
2015-08-25 15:12:54 -0500

    Fix paths

--- custom/path.zsh
+++ custom/path.zsh
@@ -2,11 +2,8 @@
 # Path-related settings
 #
 
-<<<<<<< HEAD
-=======
 # Travis
 [ -f /Users/mark/.travis/travis.sh ] && source /Users/mark/.travis/travis.sh
->>>>>>> 020f346c4619e2b5e371dc30681491a1ddb0faf4
 
 # Gradle
 export GRADLE_HOME=/Users/mark/Development/tools/gradle-2.1
@@ -19,12 +16,6 @@ export ANT_HOME=~/Development/tools/apache-ant-1.9.4
 export MAVEN_HOME=~/Development/tools/apache-maven-3.2.3
 export PATH=$PATH:/usr/local/sbin:$ANT_HOME/bin:$MAVEN_HOME/bin:/usr/local/bin
 
-<<<<<<< HEAD
-# Travis
-[ -f /Users/mark/.travis/travis.sh ] && source /Users/mark/.travis/travis.sh
-
-=======
->>>>>>> 020f346c4619e2b5e371dc30681491a1ddb0faf4
 # Oracle
 export DYLD_LIBRARY_PATH=/Users/mark/Development/tools/instantclient_11_2
 
@@ -32,11 +23,7 @@ export DYLD_LIBRARY_PATH=/Users/mark/Development/tools/instantclient_11_2
 export PGHOST=localhost
 export PATH=/Applications/Postgres.app/Contents/Versions/9.3/bin:$PATH
 
-<<<<<<< HEAD
-# GOLANG path
-=======
 # Golang path stuff
->>>>>>> 020f346c4619e2b5e371dc30681491a1ddb0faf4
 export PATH=$PATH:/usr/local/opt/go/libexec/bin
 export GOROOT=/usr/local/go
 export GOPATH=~/workspace/go
@@ -44,3 +31,6 @@ export PATH=$GOPATH/bin:$PATH
 
 # Add RVM to PATH
 export PATH="$HOME/.rvm/bin:$PATH"
+
+# add local bin before bin for things like CTAGS
+export PATH=/usr/local/bin:$PATH
