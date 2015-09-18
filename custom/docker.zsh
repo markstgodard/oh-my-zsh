f86321ebc135b0e5a7c61b5b91a1e5aebb824d56
Mark St.Godard <markstgodard@gmail.com>
2015-09-18 09:37:30 -0500

    Add docker env vars

---
+++ custom/docker.zsh
@@ -0,0 +1,4 @@
+export DOCKER_TLS_VERIFY="1"
+export DOCKER_HOST="tcp://192.168.99.100:2376"
+export DOCKER_CERT_PATH="/Users/mark/.docker/machine/machines/dev9"
+export DOCKER_MACHINE_NAME="dev9"
--- custom/env.zsh
+++ custom/env.zsh
@@ -1,7 +1,3 @@
-
-# Docker
-export DOCKER_TLS_VERIFY=1
-
 # git duet
 export GIT_DUET_ROTATE_AUTHOR=1
 
